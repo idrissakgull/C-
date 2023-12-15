e9bea24f033a65e7f6cbab6551aab4780a59215f
idris akgül <99564127+idrissakgull@users.noreply.github.com>
2023-12-15 16:42:47 +0300

    update

---
+++ Csharp Vergili maaş hesaplama/ClassLibrary4/ClassLibrary4/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sirket_dll;
+
+namespace sirket_dll
+{
+    public class maas
+    {
+
+
+        public int gelirvergisi(int gelir)
+        {
+
+            return gelir = gelir * 25 / 100;
+
+
+        }
+        public int damgavergisi(int gelir)
+        {
+
+            return gelir = gelir * 7 / 100;
+
+
+        }
+        public int emeklikisi(int gelir)
+        {
+
+            return gelir = gelir * 16 / 100;
+
+
+
+        }
+
+        public int emeklidevlet(int gelir)
+        {
+            return gelir = gelir * 20 / 100;
+
+        }
+
+
+    }
+}
---
+++ Csharp Vergili maaş hesaplama/WindowsFormsApplication27/WindowsFormsApplication27/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using sirket_dll;
+
+namespace WindowsFormsApplication27
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+   
+        maas maas = new maas();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+         int  adet =Convert.ToInt16( Microsoft.VisualBasic.Interaction.InputBox("personel sayısı?", "personel bilgi", ""));
+
+
+            for(int i = 0; i < adet; i++)
+            {
+                String ad = Microsoft.VisualBasic.Interaction.InputBox("personel adı?", "personel bilgi", "");
+                int gelir = Convert.ToInt16(Microsoft.VisualBasic.Interaction.InputBox("personel maaşı?", "personel bilgisi", ""));
+
+                int a = maas.gelirvergisi(gelir);
+                int b = maas.damgavergisi(gelir);
+                int c = maas.emeklikisi(gelir);
+                int d = maas.emeklidevlet(gelir);
+
+                int netmaas = gelir - a - b - c - d;
+
+
+                listBox1.Items.Add("adı:"+ad+"\t"+"GelirVegisi:"+ a + "\t"+"DamgaVergisi:" +b +"\t"+"EmekliKisi:"+ c +"\t"+"EmekliDevlet:"+ d+"\t"+"NetMaaş:"+netmaas);
+             
+
+
+
+
+            }
+           
+        }
+
+     
+    }
+}
